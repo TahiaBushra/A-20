675ec3d629a1bde1d895da314667f71436cea95f
TahiaBushra <bushratahia@gmail.com>
2023-12-07 21:14:23 +0600

    Set Done

--- app.js
+++ app.js
@@ -1,30 +1,102 @@
 "use strict"
 
-// for of loop (Object)
+// // for of loop (Object)
 
-const museum = {
-    name: "Bangladesh Museum",
-    buildIn: 1980,
-    location: "Dhaka",
+// const museum = {
+//     name: "Bangladesh Museum",
+//     buildIn: 1980,
+//     location: "Dhaka",
+// };
+
+// // There are 3 ways we can use---for of loop---on objects:
+
+// // 1. Object.keys()
+// const keys = Object.keys(museum);
+// for (const key of keys) {
+//     console.log(key);
+// };
+
+// //2.Object.values()
+// const values = Object.values(museum);
+// for (const value of values) {
+//     console.log(value);
+// };
+
+// // 3.Object.entries()
+// const entries = Object.entries(museum);
+// for (const [key, value] of entries) {
+//     // console.log(entry);
+//     console.log(`${key} : ${value}`);
+// };
+
+// // Data Structure : 1. Array    2. Object    3. Set    4. Map
+
+// // Set : There are 3 ways to use set . Set always shows unique result which is it eleminates duplicate number.
+
+// 1st Way
+// const mySet = new Set();
+// mySet.add(15);
+// mySet.add(50);
+// mySet.add(17);
+// mySet.add(15);
+// console.log(mySet);
+
+// 2nd Way
+// const mySet = new Set([10, 15, 23, 15, 87]);
+// console.log(mySet);
+
+// 3rd Way
+// const myNum = [2, 5, 9, 4, 2, 5, 7, 3, 1];
+// const mySet = new Set(myNum);
+// mySet.delete(5); //Delete a number from Set
+// // mySet.clear(); //To delete full Set
+// console.log(mySet);
+
+// for (const set of mySet) {
+//     console.log(set);
+// };
+
+// Example-1 : Have to creat a Set in which people with both (boat and umbrella) will be included
+// const peopleWithBoat = new Set(["Tahia", "Tamzid", "Tahsin", "Tanima"]);
+// const peopleWithUmbrella = new Set(["Tahia", "Tamzid", "Tahmid", "Tayeba"]);
+
+// function findPeopleWithBoth(setBoat, setUmbrella) {
+//     const peopleWithBoth = new Set();
+//     for (const person of setBoat) {
+//         if (setUmbrella.has(person)) {
+//             peopleWithBoth.add(person);
+//         }
+//     }
+//     return peopleWithBoth;
+// };
+// console.log(findPeopleWithBoth(peopleWithBoat, peopleWithUmbrella));
+
+// Example-2 : Have to creat a set which will show the people with only umbrella
+// const peopleWithBoat = new Set(["Tahia", "Tamzid", "Tahsin", "Tanima"]);
+// const peopleWithUmbrella = new Set(["Tahia", "Tamzid", "Tahmid", "Tayeba"]);
+
+// function findPeopleWithOnlyUmbrella(setBoat, setUmbrella) {
+//     const peopleWithOnlyUmbrella = new Set();
+//     for (const person of setUmbrella) {
+//         if (!setBoat.has(person)) {
+//             peopleWithOnlyUmbrella.add(person)
+//         }
+//     }
+//     return peopleWithOnlyUmbrella;
+// };
+// console.log(findPeopleWithOnlyUmbrella(peopleWithBoat, peopleWithUmbrella));
+
+// Example-3 : Have to creat a set which will show the people with only boat
+const peopleWithBoat = new Set(["Tahia", "Tamzid", "Tahsin", "Tanima"]);
+const peopleWithUmbrella = new Set(["Tahia", "Tamzid", "Tahmid", "Tayeba"]);
+
+function findPeopleWithOnlyUmbrella(setBoat, setUmbrella) {
+    const peopleWithOnlyBoat = new Set();
+    for (const person of setBoat) {
+        if (!setUmbrella.has(person)) {
+            peopleWithOnlyBoat.add(person)
+        }
+    }
+    return peopleWithOnlyBoat;
 };
-
-// There are 3 ways we can use---for of loop---on objects:
-
-// 1. Object.keys()                                            
-const keys = Object.keys(museum);
-for (const key of keys) {
-    console.log(key);
-};
-
-//2.Object.values()
-const values = Object.values(museum);
-for (const value of values) {
-    console.log(value);
-};
-
-// 3.Object.entries()
-const entries = Object.entries(museum);
-for (const [key, value] of entries) {
-    // console.log(entry);
-    console.log(`${key} : ${value}`);
-} 
+console.log(findPeopleWithOnlyUmbrella(peopleWithBoat, peopleWithUmbrella));
